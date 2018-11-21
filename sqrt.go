52b139116a5d97f76183893c6b1259c839be8402
Anand prabhakar <anandprabhakar0507@gmail.com>
2018-11-21 09:55:14 -0800

    Add files via upload

---
+++ sqrt.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+import "math"
+
+func main()  {
+        var a int32
+
+        a=int32(math.Sqrt(9.0))
+        
+    
+        fmt.Println("value of a:",a)
+       
+
+}
